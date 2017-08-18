4c57ccf2d05cb4a09fd3e45cfdcb3470816e9abe
Regan Laitila <github@reganlaitila.net>
2017-08-17 23:52:44 -0400

    add docs to DOMString

--- src/dom/DOMString.hx
+++ src/dom/DOMString.hx
@@ -1,3 +1,10 @@
 package dom;
 
+/**
+ *  DOMString is a UTF-16 String. As JavaScript already uses such strings, DOMString
+ *  is mapped directly to a String. Passing null to a method or parameter accepting
+ *  a DOMString typically stringifies to "null".
+ *
+ *  https://developer.mozilla.org/en-US/docs/Web/API/DOMString
+ */
 typedef DOMString = String;
